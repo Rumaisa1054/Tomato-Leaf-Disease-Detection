ef838a59a43602cd7e4db13e20e3a8bcc4ef3d9a
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-05-02 21:22:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ import numpy as np
 # Load the pre-trained model
 model1 = tf.keras.models.load_model('partly_trained3.h5')
 
-# Function to make predictions
+# Define class names
+class_names = ['Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_healthy']
+
+# Define image size
+image_size = 224
+
+# Function to preprocess and make predictions
 def predict_class(image):
     # Preprocess the image
-    image = tf.image.resize(image, (224, 224))
+    image = tf.image.resize(image, (image_size, image_size))
     image = np.expand_dims(image, axis=0)
 
     # Make predictions
     prediction = model1.predict(image)
-    return prediction
+    predicted_class_index = np.argmax(prediction)
+    predicted_class = class_names[predicted_class_index]
+    return predicted_class
 
 # Streamlit App
 st.title('Image Classification App')
